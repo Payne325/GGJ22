e6b2301be139c2144782d4eda12fecea361869e5
Payne325 <alex.payne325@gmail.com>
2022-01-29 11:17:21 +0000

    Refine throw mechanic

--- src/mover.rs
+++ src/mover.rs
@@ -49,29 +49,37 @@ impl Mover for AltMover {
 }
 
 pub struct ThrownMover {
-   thrown_speed: Vec2
+   thrown_direction: Vec2,
+   throwing_speed: f32,
+   init_time: f64
 }
 
 impl ThrownMover {
-   pub fn new(speed: Vec2) -> Self {
+   pub fn new(dir: Vec2) -> Self {
+      const THROWING_SPEED: f32 = 500.0;
+
       ThrownMover{
-         thrown_speed: speed
+         thrown_direction: dir,
+         throwing_speed: THROWING_SPEED,
+         init_time: get_time(),
       }
    }
 }
 
 impl Mover for ThrownMover {
    fn apply_movement_routine(&mut self, world: &mut World, collider: &Actor, _: &mut Vec2) {
-      
-      world.move_h(*collider, self.thrown_speed.x * get_frame_time());
-      world.move_v(*collider, self.thrown_speed.y * get_frame_time());
-      
-      let pos = world.actor_pos(*collider);
-      if self.thrown_speed.y > 1. && pos.y >= 130. {
-         self.thrown_speed.y *= -1.;
+
+      if self.throwing_speed < 1.0 {
+         self.throwing_speed = 0.0;
+         ()
       }
-      if self.thrown_speed.y < -1. && pos.y <= 40. {
-         self.thrown_speed.y *= -1.;
-      }
+
+      world.move_h(*collider, (self.thrown_direction.x * self.throwing_speed) * get_frame_time());
+      world.move_v(*collider, (self.thrown_direction.y * self.throwing_speed) * get_frame_time());
+
+      let time_delta = (get_time() - self.init_time) * 0.869;
+      let decay_rate = 1.0 - (time_delta).powf(2.0);
+
+      self.throwing_speed *= decay_rate as f32;
    }
 }
